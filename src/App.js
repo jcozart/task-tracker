c5d43d415d19a08d34a43622561794ddd417bc45
Jordan Cozart <Jordan.Cozart@matrixteam.com>
2022-12-07 08:35:50 -0600

    Added AddTask and ability to o&c  AddTask form

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import Header from "./components/Header";
 import Tasks from "./components/Tasks";
+import AddTask from "./components/AddTask";
 import { useState } from 'react';
 
 function App() {
+  const [showAddTask, setShowAddTask] = useState(true)
   const [tasks, setTasks] = useState([   
     {
         id: 1,
@@ -29,6 +31,13 @@ function App() {
         reminder: true
     },])
 
+    const addTask = (task) => {
+      const id = Math.floor(Math.random() * 10000) + 1
+      const newTask = { id, ...task }
+      setTasks([...tasks, newTask])
+
+    }
+
     const deleteTask = (id) => {
       setTasks(tasks.filter((task) => task.id !== id));
     }
@@ -40,7 +49,8 @@ function App() {
 
   return (
     <div className="container">
-      <Header />
+      <Header onAdd={() => setShowAddTask(!showAddTask)} showAdd={showAddTask}/>
+      {showAddTask && <AddTask onAdd={addTask}/>}
       {tasks.length > 0 ? <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder}/> : 'No Tasks'}
     </div>
   );
